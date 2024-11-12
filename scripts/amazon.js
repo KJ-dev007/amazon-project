7e5186347b44d7dff4881db0574dd2c27a086183
Kelvin Joseph <kelvinnjoseph77@gmail.com>
2024-11-11 22:43:07 -0800

    Move addToCart into cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 
 let productsHTML = '';
@@ -56,39 +56,28 @@ productsHTML += `
 
 });
 
-
 document.querySelector('.js-products-grid').innerHTML = productsHTML; 
+
+function updateCartQuantty(){
+  let cartQuantity = 0;
+
+
+  cart.forEach((cartItem) => {
+  cartQuantity += CartItem.quantity || 0;
+  
+  
+  });
+  document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+}
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
 button.addEventListener('click', () => {
  const productId = button.dataset.productId;
 
- let matchingItem;
-
-cart.forEach((item) => {
-if (productId=== item.productId){
-matchingItem = item;
-}
-});
-
-if(matchingItem){
-  matchingItem.quantity += 1;
-}else {
-  cart.push({
-    productId: productId,
-    quantity: 1
-  });
-};
-
-let cartQuantity = 0;
+ addToCart(productId);
+  updateCartQuantty();
 
 
-cart.forEach((item) => {
-cartQuantity += item.quantity || 0;
-
-
-});
-document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
 });
 });
 
